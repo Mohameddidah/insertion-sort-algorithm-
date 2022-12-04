ca6a34bece9f0ea57efc4d4d27808967cab249a2
Mohameddidah <109337817+Mohameddidah@users.noreply.github.com>
2022-12-04 14:48:28 +0200

    Add files via upload

---
+++ insertion project.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InsertionSort
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 4, 2, 7, 5, -1 };
+            InsertionSort(a);
+            for (int i = 0; i < a.Length; i++) 
+            {
+                Console.Write($"{a[i]}");
+            }
+            Console.ReadLine();
+        }
+        private static void InsertionSort(int[]a)
+        {
+            int cnt = a.Length;
+            int key;
+            for(int i=1; i<cnt;i++)
+            {
+                key = a[i];
+                int j = i - 1;
+                while (j>=0 && a[j]>key )
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+
+                }
+                a[j +1] = key;
+            }
+        }
+    }
+}
